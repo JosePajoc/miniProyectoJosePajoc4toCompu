94c334e27d86467b33952c7e17cceba2a804d008
Jose Pajoc <computacion.programacion.web@gmail.com>
2022-09-16 08:26:04 -0600

    creando inicio y objetos

---
+++ persona.py
@@ -0,0 +1,8 @@
+class persona():
+    #método constructor
+    def __init__(self, numero, nombre, direccion):
+        self.numero = numero
+        self.nombre = nombre
+        self.direccion = direccion
+
+    
