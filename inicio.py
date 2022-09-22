0967e584885b34f48cb7c4d0c5393300922c9a69
Jose Pajoc <computacion.programacion.web@gmail.com>
2022-09-22 12:18:09 -0600

    funcion eliminar contacto

--- inicio.py
+++ inicio.py
@@ -52,6 +52,28 @@ def modificarContacto(nombre):
         else:
             print("Dato no encontrado...")
 
+def eliminarContacto(nombre):
+    if len(misContactos) == 0:
+        print("La lista esta vacía, no hay contactos que buscar...")
+    else:
+        encontrado = False
+        posicion = None
+        for i in range(len(misContactos)):
+            if misContactos[i].verNombre() == nombre:
+                posicion = i
+                encontrado = True
+                break
+            else:
+                encontrado = False
+        if encontrado:
+            misContactos.pop(posicion)
+            print("Dato eliminado con éxito...")
+        else:
+            print("Dato no encontrado...")
+
+def crearReporte():
+    print("Creando reporte HTML")
+
 def main():
     op = 0
     while op != 7:
@@ -77,6 +99,15 @@ def main():
         elif op == 4:
             nombre = input("Ingrese el nombre del contacto: ")
             modificarContacto(nombre)
+        elif op == 5:
+            nombre = input("Ingrese el nombre del contacto: ")
+            eliminarContacto(nombre)
+        elif op == 6:
+            crearReporte()
+        elif op == 7:
+            print("Programa finalizado...")
+        else:
+            print("Opción incorrecta..")
 
 #iniciar programa
 main()
